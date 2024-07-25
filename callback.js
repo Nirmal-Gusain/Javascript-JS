7898a8018eadac7391a1cfdf1938bbc9def19f46
Nirmal Gusain <155016618+Nirmal-Gusain@users.noreply.github.com>
2024-07-25 19:24:59 +0530

    update

--- callback.js
+++ callback.js
@@ -1,3 +1,5 @@
+                            // CALLBACK FUNCTION
+
 // function hello(a){
 //     console.log(a)
 //     console.log("hello1")
@@ -7,6 +9,9 @@
 //     console.log("fun1")
 // }
 // fun(hello(5))
+
+                            // MAP FUNCTION
+
 
 // let ar = [1,2,3,4,5]
 // const double=function(a){
@@ -27,11 +32,40 @@
 //     return a*a
 // })
 
+                        // UPPER CASE THE GIVEN ARRAY
 
 // let arr = ["rahul","sumit","rohan"]
 // let upper =arr.map((a)=>a.toUpperCase())
 // console.log(upper)
 
-let ar = [2,5,4,9,3,10,8]
+                        // DOUBLE THE NUMBER IN ARRAY
 
-let largest = ar.map((a)=>ar)
+// let ar = [1,2,3,4,5]
+
+// ar.map((a)=>{
+//     console.log(a*a)
+// })
+
+                        // double the number if greater than 25
+
+// let arr = [10,20,30,40,50]
+// let newarr = arr.map((a)=>{
+//     if(a>20){
+//         return a*2
+//     }else{
+//         return a
+//     }
+// })
+// console.log(newarr)
+
+
+                        // FIND HIGHEST NUMBER 
+
+// let arr = [5,2,40,10,8,3]
+// let max = 0
+// arr.map((a)=>{
+//     if(a>max){
+//         max=a
+//     }
+// })
+// console.log(max)
