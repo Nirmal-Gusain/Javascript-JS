493ecaf002de6e0dd3c972ac0aaae7c72393d84d
Nirmal Gusain <155016618+Nirmal-Gusain@users.noreply.github.com>
2024-08-01 19:16:17 +0530

    Update

--- promise.js
+++ promise.js
@@ -14,3 +14,25 @@ myPromise.then((value)=>{
 myPromise.catch((err)=>{
     console.log(err)
 })
+
+
+let store = {
+    water:true,
+    tealeaves:true,
+    suger:true,
+    milk:true
+}
+function tea(res,rej){
+    if(store.water && store.tealeaves && store.suger && store.milk){
+        res("Tea is Ready")
+    }else{
+        rej("Tea is not made")
+    }
+}
+let newPromise = new Promise(tea)
+newPromise.then((value)=>{
+    console.log(value)
+})
+newPromise.catch((error)=>{
+    console.log(error)
+})
